eac45cbcdc3b7546a70f337ee1fc1edd4727ea61
Medeenat22 <olaniyanjoke@yahoo.com>
2023-07-17 15:06:12 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,22 +6,13 @@
 
 int main(void)
 {
-	char c;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
 
-	char d;
-
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z') {
-			putchar (d);
-			d++;
-		}
+for (i = 0; i < 52; i++)
+{
+	putchar(alp[i]);
+}
 	putchar('\n');
 	return (0);
 }
