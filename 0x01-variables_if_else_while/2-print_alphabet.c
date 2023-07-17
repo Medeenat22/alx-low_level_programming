a40c5eff39f2a9c7da8998dcdd64415bdd1021b5
Medeenat22 <olaniyanjoke@yahoo.com>
2023-07-17 14:38:20 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - still entry point
+ * Return: 0 is still success
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
