70f298cfc8cef6e583d9f804f9a9fe51b7d89801
Medeenat22 <olaniyanjoke@yahoo.com>
2023-08-14 01:26:39 -0700

    number speaks for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for a digit (0 to 9)
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 37; a < 47; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
